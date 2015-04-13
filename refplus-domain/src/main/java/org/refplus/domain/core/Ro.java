2ffad261d8a4e6167314734f1fd58bc0f1ed522c
erikvaningen <erikvaningen@gmail.com>
2015-04-13 11:54:46 +0200

    stable version with AttibuteConcept as alternative

--- refplus-domain/src/main/java/org/refplus/domain/core/Ro.java
+++ refplus-domain/src/main/java/org/refplus/domain/core/Ro.java
@@ -19,8 +19,39 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode
 public class Ro {
 
+	/**
+	 * This is the 1st alternative to the combination codeList and multiLingualStringList.
+	 * 
+	 * The AttributeConcepts of RefPlus can be dynamically created in the system and should only be edited by a metadata
+	 * administrator. They could be loaded through a metadata file in CSV, JSON or XML.
+	 * 
+	 * This approach solves also the problem of accessing easily a certain attribute. Imagine you have names and long
+	 * names. Now they are listed as 2 attributes in the multiLingualStringList. You would then have to find it, while
+	 * pulling it from attributeMap1 is a one command action.
+	 * 
+	 * 
+	 */
+	// private Map<AttributeConcept, Attribute> attributeMap1;
+
+	/**
+	 * This is the 2nd alternative to the combination codeList and multiLingualStringList.
+	 * 
+	 * AttributeConcept = {name, longName, code, value}
+	 * 
+	 * The disadvantage of this approach is that you cannot give names dynamically, so you cannot add a new attribute
+	 * like iso3 or something, because every attribute is a class in Java. Therefore this approach is not enough
+	 * flexible and the attributeMap1 is therefore better.
+	 * 
+	 * 
+	 */
+	// private Map<Class<? extends AttributeConcept>, Attribute> attributeMap2;
+
+	/**
+	 * This codeList is not to be confused with CodeList
+	 */
 	private List<Code> codeList;
 	private List<MultiLingualString> multiLingualStringList;
+	private List<String> valueList;
 
 	public Ro(Code code, MultiLingualString mls) {
 		codeList = new ArrayList<Code>();
